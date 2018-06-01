2b948ee45db717cf8a052b3f74d715c6d4ab5a4b
Pelumi Longe <longe.pelumi@yahoo.com>
2018-06-01 16:49:06 +0100

    fix the linting issues of the server test files

--- test/user/auth.js
+++ test/user/auth.js
@@ -7,104 +7,105 @@ require('dotenv').config();
 const User = mongoose.model('User');
 
 const { expect } = require('chai');
+
 const request = supertest(app);
 
 describe('Signin Tests', () => {
-    const userDetails = {
+  const userDetails = {
+    email: 'jeremiaholufayo@gmail.com',
+    password: 'olufayo'
+  };
+
+    // run once before tests
+  before((done) => {
+    mongoose.connect(process.env.CFH_TESTDB, () => {
+      const user = new User(userDetails);
+      user.save();
+      done();
+    });
+  });
+
+  // run once after tests
+  after((done) => {
+    mongoose.connection.db.dropDatabase(() => {
+      done();
+    });
+  });
+
+  it('should not signin a user without email', (done) => {
+    request
+      .post('/api/auth/login')
+      .send({
+        password: 'olufayo'
+      })
+      .end((err, res) => {
+        expect(res.statusCode).to.equal(404);
+        expect(res.body).to.have.property('error')
+          .eql('Your Email Address is required');
+        // expect(res.body[0].message).to.equal('Your Email Address is required');
+        // expect(res.body[1].message).to.equal('Provide a valid Email Address');
+        done();
+      });
+  });
+
+  it('should not signin a user without password', (done) => {
+    request
+      .post('/api/auth/login')
+      .send({
+        email: 'jeremiaholufayo@gmail.com'
+      })
+      .end((err, res) => {
+        expect(res.statusCode).to.equal(404);
+        expect(res.body).to.have.property('error')
+          .eql('Your Password is required');
+        // expect(res.body).to.be.an('array');
+        // expect(res.body[0].message).to.equal('Your Password is required')
+        done();
+      });
+  });
+
+  it('should not signin a user with an invalid email', (done) => {
+    request
+      .post('/api/auth/login')
+      .send({
+        email: 'jeremiaholufayogmail.com'
+      })
+      .end((err, res) => {
+        expect(res.statusCode).to.equal(404);
+        expect(res.body).to.have.property('error')
+          .eql('Provide a valid Email Address');
+        // expect(res.body).to.be.an('array');
+        // expect(res.body).to.equal('Provide a valid Email Address')
+        done();
+      });
+  });
+
+  it('should not signin a user with wrong details', (done) => {
+    request
+      .post('/api/auth/login')
+      .send({
+        email: 'jeremiaholufayo@gmail.com',
+        password: 'jerry'
+      })
+      .end((err, res) => {
+        expect(res.statusCode).to.equal(400);
+        expect(res.body.error).to.equal('Username or Password Incorrect');
+        done();
+      });
+  });
+
+  it('should signin a user with correct details', (done) => {
+    request
+      .post('/api/auth/login')
+      .send({
         email: 'jeremiaholufayo@gmail.com',
         password: 'olufayo'
-    };
-    
-    //run once before tests
-    before((done) => {
-        mongoose.connect(process.env.CFH_TESTDB, () => {
-            const user = new User(userDetails);
-            user.save();
-            done();
-        });
-    });
-
-    //run once after tests
-    after((done) => {
-        mongoose.connection.db.dropDatabase(() => {
-            done();
-        });
-    });
-
-    it('should not signin a user without email', (done) => {
-        request
-          .post('/api/auth/login')
-          .send({
-            password: 'olufayo'
-          })
-          .end((err, res) => {
-            expect(res.statusCode).to.equal(404);
-            expect(res.body).to.have.property('error')
-                .eql('Your Email Address is required');
-            // expect(res.body[0].message).to.equal('Your Email Address is required');
-            // expect(res.body[1].message).to.equal('Provide a valid Email Address');            
-            done();
-          });
-    });
-
-    it('should not signin a user without password', (done) => {
-        request
-          .post('/api/auth/login')
-          .send({
-            email: 'jeremiaholufayo@gmail.com'
-          })
-          .end((err, res) => {
-            expect(res.statusCode).to.equal(404);
-            expect(res.body).to.have.property('error')
-                .eql('Your Password is required');
-            // expect(res.body).to.be.an('array');
-            // expect(res.body[0].message).to.equal('Your Password is required')          
-            done();
-          });
-    });
-
-    it('should not signin a user with an invalid email', (done) => {
-        request
-          .post('/api/auth/login')
-          .send({
-            email: 'jeremiaholufayogmail.com'
-          })
-          .end((err, res) => {
-            expect(res.statusCode).to.equal(404);
-            expect(res.body).to.have.property('error')
-                .eql('Provide a valid Email Address');
-            // expect(res.body).to.be.an('array');
-            // expect(res.body).to.equal('Provide a valid Email Address')          
-            done();
-          });
-    });
-
-    it('should not signin a user with wrong details', (done) => {
-        request
-          .post('/api/auth/login')
-          .send({
-            email: 'jeremiaholufayo@gmail.com',
-            password: 'jerry'
-          })
-          .end((err, res) => {
-            expect(res.statusCode).to.equal(400);
-            expect(res.body.error).to.equal('Username or Password Incorrect');
-            done();
-          });
-    });
-
-    it('should signin a user with correct details', (done) => {
-        request
-          .post('/api/auth/login')
-          .send({
-            email: 'jeremiaholufayo@gmail.com',
-            password: 'olufayo'
-          })
-          .end((err, res) => {
-            expect(res.statusCode).to.equal(200);
-            expect(res.body.message).to.equal('Successfully SignIn');
-            expect(res.body.token).to.not.equal(null);
-            done();
-          });
-    });
-})
+      })
+      .end((err, res) => {
+        expect(res.statusCode).to.equal(200);
+        expect(res.body.message).to.equal('Successfully SignIn');
+        expect(res.body.token).to.not.equal(null);
+        done();
+      });
+  });
+});
