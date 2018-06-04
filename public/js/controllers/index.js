818aac58b242ece307141cc84d810ecdfe7ae1a4
Pelumi Longe <longe.pelumi@yahoo.com>
2018-06-04 19:41:09 +0100

    upload image or choose an avatar while signing up

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -2,8 +2,9 @@ angular.module('mean.system')
 .controller('IndexController', ['$scope', '$http', 'Global', '$location', 'socket', 'game', 'AvatarService', function ($scope, $http, Global, $location, socket, game, AvatarService) {
     $scope.global = Global;
 
-    $scope.signup = function(){
-      const userDetails = $scope.user;
+    // 
+    const sendSignUPRequest = (userDetails) => {
+      // send the post request to the server
       $http.post('/api/auth/signup', userDetails)
       .then(
         (response) => {
@@ -12,8 +13,75 @@ angular.module('mean.system')
           $location.path('/');
         },
         (error) => {
+          // display server errors
           $scope.hasError = error.data
         })
+    };
+
+    $scope.previewImage = () => {
+      // collect image chosen from the signup form
+      const imageFile = $('#userImage').prop('files')[0];
+      if (imageFile) {
+        const fileReader = new FileReader();
+        fileReader.readAsDataURL(imageFile);
+        fileReader.onload = event => {
+          // set the preview
+          $scope.imagePreview = event.target.result;
+        };
+      }
+    };
+
+    $scope.signup = function(){
+      // collect user details from the signup form
+      const userDetails = $scope.user;
+      const { name, email, password } = userDetails;
+      $scope.hasError = {};
+
+      // collect image chosen from the signup form
+      const imageFile = $('#userImage').prop('files')[0];
+      const error = {}
+      let imageUrl;
+
+      // validate name, email and password is available
+      Object.entries({ name, email, password }).forEach(([key, value]) => {
+        if (!value || value === '') {
+          error[key] = `Please supply your ${key}`;
+        }
+      });
+      // validate that either an avatar or an image upload is chosen
+      if (!imageFile && !userDetails.avatar) {
+        error.avatar = 'Please choose an avatar or upload an image';
+      }
+
+      // if any validation fails, display the error
+      if (Object.keys(error).length !== 0) {
+        $scope.hasError = error;
+      } else {
+        // upload the image to cloudinary
+        if (imageFile) {
+          const formData = new FormData();
+          formData.append('file', imageFile);
+          formData.append('upload_preset', 'lpxun7n2');
+          $.ajax({
+            url: 'https://api.cloudinary.com/v1_1/longe/image/upload',
+            data: formData,
+            type: 'POST',
+            cache : false,
+            processData: false,
+            contentType: false,
+            dataType: 'json',
+          })
+            .then((res) => {
+              imageUrl = res.secure_url;
+              userDetails.avatar = imageUrl;
+
+              return sendSignUPRequest(userDetails);
+            });
+        } else {
+          // send the post request to the server
+          return sendSignUPRequest(userDetails);
+        }
+      }
     };
 
     // login a user
@@ -28,10 +96,10 @@ angular.module('mean.system')
           $location.path('/');
         }
       },
-    (response) => {
-      const { error } = response.data
-      $scope.hasError = error;
-    })
+      (response) => {
+        const { error } = response.data
+        $scope.hasError = error;
+      })
     };
 
     $scope.playAsGuest = function() {
