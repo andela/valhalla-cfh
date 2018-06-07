2c690310f41a2503421b1540b10b20bf90e68528
Nedson202 <simeonsamson4@gmail..com>
2018-06-07 12:04:03 +0100

    Change token secret to env variable

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -9,6 +9,7 @@ const avatars = require('./avatars').all();
 const nodemailer = require('nodemailer');
 
 const User = mongoose.model('User');
+require('dotenv').config();
 
 /**
  * Auth callback
@@ -261,7 +262,7 @@ exports.registerUser = (req, res) => {
         email: createdUser.email
       };
 
-      const token = jwt.sign(userData, 'secret');
+      const token = jwt.sign(userData, process.env.SECRET);
 
       return res.status(201).json({
         message: 'User successfully registered',
@@ -300,7 +301,7 @@ exports.login = (req, res) => {
         id: user.id
       };
       // Create token
-      const token = jwt.sign(userData, 'secretkey', { expiresIn: '5h' });
+      const token = jwt.sign(userData, process.env.SECRET, { expiresIn: '5h' });
       return res.status(200).json({
         token,
         message: 'Successfully SignIn',
