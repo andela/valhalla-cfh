397f16be2834b9c043c8c019756f654c6e19c1f4
Nedson202 <simeonsamson4@gmail..com>
2018-06-12 15:18:11 +0100

    Add methods for user authentication

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -1,144 +1,183 @@
 angular.module('mean.system')
-.controller('IndexController', ['$scope', '$http', 'Global', '$location', 'socket', 'game', 'AvatarService', function ($scope, $http, Global, $location, socket, game, AvatarService) {
+  .controller('IndexController', ['$scope', '$http', 'Global', '$location', 'socket', 'game', 'AvatarService', function ($scope, $http, Global, $location, socket, game, AvatarService) {
     $scope.global = Global;
     $scope.gameWithCustom = 'false';
 
-    const sendSignUPRequest = (userDetails) => {
+    // Run validation on user input
+    $scope.validator = () => {
+      const userDetails = $scope.user;
+      const { name, email, password } = userDetails;
+      $scope.hasError = {};
       // send the post request to the server
-      $http.post('/api/auth/signup', userDetails)
-      .then(
-        (response) => {
-          const { token, message } = response.data;
-          localStorage.setItem('token', token);
-          $scope.showOptions = false;
-          $('#closeSignUp').click();
-          toastr.success(message)
-          $location.path('/');
-        },
-        (errors) => {
+      $http.post('/api/validator', userDetails)
+        .then(
+          (response) => {
+            // toggle modal if action is successful
+            $('#closeSignUp').click();
+            $('#openSecondSignUp').click();
+          },
+          (errors) => {
           // display errors if input is empty or invalid
-          errors.data.map(err => {
-            toastr.error(err);
-          })
-        })
+            $scope.hasError = errors.data
+          }
+);
     };
 
     $scope.previewImage = () => {
       // collect image chosen from the signup form
+      // const imageFile = '';
       const imageFile = $('#userImage').prop('files')[0];
+      
       if (imageFile) {
+        document.getElementById("addHide").classList.add('hide');
+        document.getElementById("removeHide").classList.add('rounded-circle');
+        document.getElementById("removeHide").classList.remove('hide');
+        // $('#addHide').classList.add('hide');
         const fileReader = new FileReader();
         fileReader.readAsDataURL(imageFile);
-        fileReader.onload = event => {
+        fileReader.onload = (event) => {
           // set the preview
           $scope.imagePreview = event.target.result;
         };
       }
     };
 
-    // open modal for start Game
-    $scope.startGameModal = function(term){
-        // setting the buttons
-        const startButton= `<a href="/play"><button type="button" class="btn btn-md" style="background: aqua">Start</button></a>`;
-        const idStartButton = `<a href="/play?custom"><button type="button" class="btn btn-md" style="background: aqua">Start</button></a>`;
-        const closeModal = `<button id="closeModal" data-dismiss="modal" type="button" class="btn btn-md" style="background: rgb(255, 136, 0)">Close</button>`;
-        // call the modal and append their attributes
-        const infoModal = $('#infoModal');
-        infoModal.find('.modal-title')
-          .text('Info!!!');
-        infoModal.find('.modal-body')
-         .text('You are about to start a new game. Click button to start');
-         
-         if(term === 'custom') { 
-            $( ".button" ).empty();
-            infoModal.find('.button').append(idStartButton, closeModal);
-        }
-        if(term === undefined){
-          $( ".button" ).empty();
-            infoModal.find('.button').append(startButton, closeModal);
-        }
-        infoModal.modal('show');
-     
+    // toggle option to select avatar or upload an image
+    $scope.toggleImageSelect = () => {
+      const option = $scope.user.option;
+      if(option === 'Upload image') {
+        document.getElementById('image-upload').classList.remove('hide');
+        document.getElementById('avatar-select').classList.add('hide');
+        document.getElementById('toggle-button').classList.remove('hide');
+        $scope.user.avatar = null;
+        $scope.hasError = null;
+      } else {
+        document.getElementById('avatar-select').classList.remove('hide');
+        document.getElementById('image-upload').classList.add('hide');
+        document.getElementById('toggle-button').classList.remove('hide');
+        $scope.hasError = null;
+      }
     }
 
-    $scope.signup = function(){
+    // open modal for start Game
+    $scope.startGameModal = function (term) {
+      // setting the buttons
+      const startButton = '<a href="/play"><button type="button" class="btn btn-md" style="background: aqua">Start</button></a>';
+      const idStartButton = '<a href="/play?custom"><button type="button" class="btn btn-md" style="background: aqua">Start</button></a>';
+      const closeModal = '<button id="closeModal" data-dismiss="modal" type="button" class="btn btn-md" style="background: rgb(255, 136, 0)">Close</button>';
+      // call the modal and append their attributes
+      const infoModal = $('#infoModal');
+      infoModal.find('.modal-title')
+        .text('Info!!!');
+      infoModal.find('.modal-body')
+        .text('You are about to start a new game. Click button to start');
+
+      if (term === 'custom') {
+        $('.button').empty();
+        infoModal.find('.button').append(idStartButton, closeModal);
+      }
+      if (term === undefined) {
+        $('.button').empty();
+        infoModal.find('.button').append(startButton, closeModal);
+      }
+      infoModal.modal('show');
+    };
+    
+    const finishSignup = (userDetails) => {
+      $http.post('api/auth/signup', userDetails).then(
+      (response) => {
+          const { token } = response.data;
+          if (token) {
+            localStorage.setItem('token', token);
+            $scope.showOptions = false;
+            $('#closeSecondModal').click();
+            $scope.showSuccessMessage = response.data.message
+            $('#openSuccessModal').click();
+          }
+        },
+        (response) => {
+        }
+      );
+    };
+
+    $scope.signup = function () {
       // collect user details from the signup form
       const userDetails = $scope.user;
-      const { name, email, password } = userDetails;
-      $scope.hasError = {};
-      console.log($scope.user);
+      const { name, email, password, avatar, userImage } = userDetails;
 
       // collect image chosen from the signup form
       const imageFile = $('#userImage').prop('files')[0];
-      const errors = [];
       let imageUrl;
 
-      // validate name, email and password is available
-      Object.entries({ name, email, password }).forEach(([key, value]) => {
-        if (!value || value === '') {
-          errors.push(`Please supply your ${key}`);
-        }
-      });
-      // validate that either an avatar or an image upload is chosen
-      if (!imageFile && !userDetails.avatar) {
-        errors.push('Please choose an avatar or upload an image');
+      if(!userImage && !avatar) {
+        return $scope.hasError = {'error': 'Sorry you need to upload an image or choose an avatar'}
       }
-
-      // if any validation fails, display the error
-      if (Object.keys(errors).length !== 0) {
-        // Display errors if input is empty
-        errors.map(err => {
-          toastr.error(err)
+      
+      document.getElementById('has-error').classList.add('hide');
+      if (imageFile) {
+        const formData = new FormData();
+        formData.append('file', imageFile);
+        formData.append('upload_preset', 'lpxun7n2');
+        $.ajax({
+          url: 'https://api.cloudinary.com/v1_1/longe/image/upload',
+          data: formData,
+          type: 'POST',
+          cache: false,
+          processData: false,
+          contentType: false,
+          dataType: 'json',
         })
-      } else {
-        // upload the image to cloudinary
-        if (imageFile) {
-          const formData = new FormData();
-          formData.append('file', imageFile);
-          formData.append('upload_preset', 'lpxun7n2');
-          $.ajax({
-            url: 'https://api.cloudinary.com/v1_1/longe/image/upload',
-            data: formData,
-            type: 'POST',
-            cache : false,
-            processData: false,
-            contentType: false,
-            dataType: 'json',
-          })
-            .then((res) => {
-              imageUrl = res.secure_url;
-              userDetails.avatar = imageUrl;
+          .then((res) => {
+            imageUrl = res.secure_url;
+            userDetails.avatar = imageUrl;
 
-              return sendSignUPRequest(userDetails);
-            });
-        } else {
-          // send the post request to the server
-          return sendSignUPRequest(userDetails);
-        }
+            return finishSignup(userDetails);
+          });
+      } else {
+        // send the post request to the server
+        return finishSignup(userDetails);
       }
     };
 
     // login a user
-    $scope.login = function() {
+    $scope.login = function () {
       $http.post('api/auth/login', {
         email: $scope.email,
         password: $scope.password
-      }).then((response) => {
-        const token = response.data.token;
-        if(token){
-          localStorage.setItem('token', token);
-          $scope.showOptions = false;
-          $('#closeLogin').click();
-          toastr.success('Successfully signed in');
+      }).then(
+(         response) => {
+          const token = response.data.token;
+          if (token) {
+            localStorage.setItem('token', token);
+            $scope.showOptions = false;
+            $('#closeLogin').click();
+            toastr.options = {
+              "closeButton": false,
+              "debug": false,
+              "newestOnTop": false,
+              "progressBar": false,
+              "positionClass": "toast-top-right",
+              "preventDuplicates": false,
+              "onclick": null,
+              "showDuration": "100",
+              "hideDuration": "1000",
+              "timeOut": "50000",
+              "extendedTimeOut": "1000",
+              "showEasing": "swing",
+              "hideEasing": "linear",
+              "showMethod": "fadeIn",
+              "hideMethod": "fadeOut"
+            }
+            toastr.success('Successfully signed in');
         }
       },
-      (response) => {
-        const { error } = response.data
-        toastr.error(error);
-      })
+      (errors) => {
+        $scope.hasError = errors.data;
+      }
+);
     };
 
-    $scope.playAsGuest = function() {
+    $scope.playAsGuest = function () {
       game.joinGame();
       $location.path('/app');
     };
@@ -146,15 +185,14 @@ angular.module('mean.system')
     $scope.showError = function() {
       if ($location.search().error) {
         return $location.search().error;
-      } else {
+      } 
         return false;
-      }
+      
     };
 
     $scope.avatars = [];
     AvatarService.getAvatars()
-      .then(function(data) {
+      .then((data) => {
         $scope.avatars = data;
       });
-
-}]);
+  }]);
