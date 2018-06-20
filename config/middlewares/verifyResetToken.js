3869d8ad661b3958ccc6957a52d73100ee78ffee
Nedson202 <simeonsamson4@gmail..com>
2018-06-20 20:16:24 +0100

    Add middleware to verify token from address bar

--- config/middlewares/verifyResetToken.js
+++ config/middlewares/verifyResetToken.js
@@ -5,15 +5,30 @@ require('dotenv').config();
 exports.verifyResetToken = (req, res, next) => {
   // Get token value from headers
   const { token } = req.params;
-  // return console.log(token);
 
-  jwt.verify(token, process.env.SECRET, (error, decoded) => {
+  jwt.verify(token, process.env.SECRET, (error) => {
     if (error) {
       res.redirect('/#!/password-reset-error');
     } else {
-      req.decoded = decoded;
+      req.decoded = token;
       next();
     }
+  });
+};
+
+exports.resetToken = (req, res, next) => {
+  // Get token value from headers
+  const { token } = req.body;
+  const jwtToken = token.split('?')[1];
+
+  jwt.verify(jwtToken, process.env.SECRET, (error, decoded) => {
+    if (error) {
+      return res.status(400).json({
+        message: 'Sorry, an error occurred please use the forgot password section'
+      });
+    }
+    req.decoded = decoded;
+    next();
   });
 };
 
